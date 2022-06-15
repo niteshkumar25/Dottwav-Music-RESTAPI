e5015ac8df5ccc5ab01d9b4e9baac2105606879b
Nitesh Kumar <82771047+niteshkumar25@users.noreply.github.com>
2022-06-15 12:38:51 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 userSchema = new mongoose.Schema({
-    username: { type: String, require: true, unique: true },
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     playlists: { type: [String], default: [] },
     isAdmin: { type: Boolean, default: false },
 
